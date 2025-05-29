61465488a6d9c473eef966ef608dd77b109129ae
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-29 14:00:30 +0300

    class & object

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public void displayInfo() {
+        System.out.println("Marka: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Yıl: " + year);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.brand = "Mercedes";
+        myCar.model = "C200";
+        myCar.year = 2023;
+
+        myCar.displayInfo();
     }
 }

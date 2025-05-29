2ab040c21b6027ccdb8b3aeb4491c5bcd53493ad
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-29 14:12:35 +0300

    encapsulation

--- src/Car.java
+++ src/Car.java
@@ -1,17 +1,41 @@
 public class Car {
-    String brand;
-    String model;
-    int year;
+    private String brand;
+    private String model;
+    private int year;
 
-    public Car(String brand, String model, int year) {
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
         this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
         this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
         this.year = year;
     }
 
-    public void displayInfo() {
-        System.out.println("Marka: " + brand);
-        System.out.println("Model: " + model);
-        System.out.println("Yıl: " + year);
-    }
+    //    public Car(String brand, String model, int year) {
+//        this.brand = brand;
+//        this.model = model;
+//        this.year = year;
+//    }
+
+//    public void displayInfo() {
+//        System.out.println("Marka: " + brand);
+//        System.out.println("Model: " + model);
+//        System.out.println("Yıl: " + year);
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Car myCar = new Car("Mercedes", "C200", 2023);
+        Car myCar = new Car();
 
-        myCar.displayInfo();
+        myCar.setBrand("Mercedes");
+        myCar.setModel("C200");
+        myCar.setYear(2023);
+
+        System.out.println("Marka: " + myCar.getBrand());
+        System.out.println("Model: " + myCar.getModel());
+        System.out.println("Yıl: " + myCar.getYear());
     }
 }

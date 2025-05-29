60a828a2d2eb49e18cef3003ef6bc3e53c11c41b
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-29 14:03:46 +0300

    add constructor

--- src/Car.java
+++ src/Car.java
@@ -3,6 +3,12 @@ public class Car {
     String model;
     int year;
 
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
     public void displayInfo() {
         System.out.println("Marka: " + brand);
         System.out.println("Model: " + model);
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Car myCar = new Car();
-
-        myCar.brand = "Mercedes";
-        myCar.model = "C200";
-        myCar.year = 2023;
+        Car myCar = new Car("Mercedes", "C200", 2023);
 
         myCar.displayInfo();
     }

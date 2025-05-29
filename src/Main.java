65fb5daad75f9ae56edf482ed391222b8da01475
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-29 14:28:26 +0300

    polymorphism

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Car myCar = new Car();
+//        Car myCar = new Car();
+//
+//        myCar.setBrand("Mercedes");
+//        myCar.setModel("C200");
+//        myCar.setYear(2023);
+//        myCar.setSpeed(120);
+//
+//        System.out.println("Marka: " + myCar.getBrand());
+//        System.out.println("Model: " + myCar.getModel());
+//        System.out.println("Yıl: " + myCar.getYear());
+//        System.out.println("Arabanın hızı: " + myCar.getSpeed() + " km/s");
+//        myCar.start();
 
-        myCar.setBrand("Mercedes");
-        myCar.setModel("C200");
-        myCar.setYear(2023);
-        myCar.setSpeed(120);
+        Animal cat = new Cat();
+        Animal dog = new Dog();
 
-        System.out.println("Marka: " + myCar.getBrand());
-        System.out.println("Model: " + myCar.getModel());
-        System.out.println("Yıl: " + myCar.getYear());
-        System.out.println("Arabanın hızı: " + myCar.getSpeed() + " km/s");
-        myCar.start();
+        cat.makeSound();
+        dog.makeSound();
     }
 }
